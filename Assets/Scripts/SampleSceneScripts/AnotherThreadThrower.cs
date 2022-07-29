a2af3507820a949a9e53960577c02331f7e5df6b
Gevorg <gulkanyangk@mail.ru>
2022-07-29 14:43:40 +0300

    InitCommint and added MainThreadThrower.cs

---
+++ Assets/Scripts/SampleSceneScripts/AnotherThreadThrower.cs
@@ -0,0 +1,7 @@
+﻿namespace LoggerAsset.SampleSceneScripts
+{
+    public class AnotherThreadThrower
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SampleSceneScripts/MainThreadThrower.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace LoggerAsset.SampleSceneScripts
+{
+    public class MainThreadThrower : MonoBehaviour
+    {
+        private const float TimeBetweenMessages = 0.5f;
+        
+        private float _timer;
+
+        private void Update()
+        {
+            _timer += Time.deltaTime;
+            if (_timer > TimeBetweenMessages)
+            {
+                _timer = 0;
+                LogMain();
+            }
+        }
+
+        private void LogMain()
+        {
+            int randomValue = Random.Range(0, 4);
+
+            switch (randomValue)
+            {
+                case 0:
+                    Debug.Log("Log from MAIN THREAD");
+                    break;
+                case 1:
+                    Debug.LogWarning("Warning from MAIN THREAD");
+                    break;
+                case 2:
+                    Debug.LogError("Error from MAIN THREAD");
+                    break;
+                case 3:
+                    throw new NullReferenceException();
+                    break;
+            }
+        }
+    }
+}
