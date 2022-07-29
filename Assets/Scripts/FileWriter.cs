8dfe2d5a19a182d6564baef5303f396e96166604
Gevorg <gulkanyangk@mail.ru>
2022-07-29 15:42:17 +0300

    Added FileWriter.cs, dateTime

---
+++ Assets/Scripts/FileWriter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace LoggerAsset
+{
+    public class FileWriter
+    {
+        private const string DateFormat = "yyyy-MM-dd";
+        
+        private string _folder;
+        private string _filePath;
+
+        public FileWriter(string folder)
+        {
+            _folder = folder;
+            ManagePath();
+        }
+
+        private void ManagePath()
+        {
+            _filePath = $"{_folder}/{DateTime.UtcNow.ToString(DateFormat)}.log";
+        }
+
+        public void Write(string message)
+        {
+            using (FileStream fileStream = File.Open(_filePath, FileMode.Append, FileAccess.Write, FileShare.Read))
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(message);
+                fileStream.Write(bytes, 0, bytes.Length);
+            }
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Logger.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace LoggerAsset
+{
+    public class Logger : MonoBehaviour
+    {
+        private FileWriter _fileWriter;
+        private string _workDirectory;
+
+        private void Awake()
+        {
+            _workDirectory = $"{Application.persistentDataPath}/Logs";
+            
+            if (Directory.Exists(_workDirectory) == false)
+                Directory.CreateDirectory(_workDirectory);
+
+            _fileWriter = new FileWriter(_workDirectory);
+            Application.logMessageReceivedThreaded += OnLogMessageReceived;
+        }
+
+        private void OnLogMessageReceived(string condition, string stacktrace, LogType type)
+        {
+            _fileWriter.Write(condition);
+        }
+
+        private void Update()
+        {
+#if UNITY_EDITOR
+            if(Input.GetKeyUp(KeyCode.L))
+                UnityEditor.EditorUtility.RevealInFinder(_workDirectory);
+#endif
+        }
+    }
+}
